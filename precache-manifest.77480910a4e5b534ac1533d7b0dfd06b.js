8741beb585dd6b02a0578a8bc651d40716cad0c8
davit voskanyan <davitvoskanyan@workfront.com>
2020-01-04 22:17:52 +0400

    added portfolio

---
+++ precache-manifest.77480910a4e5b534ac1533d7b0dfd06b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686ceac5d4f07e6ea6031119865c3dcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5257a8476b3060679c1",
+    "url": "/static/css/main.a9513851.chunk.css"
+  },
+  {
+    "revision": "2ceab24ab4ec138cfeea",
+    "url": "/static/js/2.890539fd.chunk.js"
+  },
+  {
+    "revision": "fb76c9f8f8182087f9c180bc5fcbff28",
+    "url": "/static/js/2.890539fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5257a8476b3060679c1",
+    "url": "/static/js/main.a95b43c2.chunk.js"
+  },
+  {
+    "revision": "2a7b9d97c7dc908bd3cd",
+    "url": "/static/js/runtime-main.272b2eac.js"
+  }
+]);
